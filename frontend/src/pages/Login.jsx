6648ad20b0f74942d97a7f5de82f24e171ac131c
Athoi1040 <athoi35-1040@diu.edu.be>
2026-01-04 12:48:09 +0600

    c

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -82,7 +82,8 @@ const Login = ({ setIsLoggedIn }) => {
         </div>
         <div className="login-right">
           <img
-            src="https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80"
+           
+            src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80"
             alt="Login"
           />
         </div>
